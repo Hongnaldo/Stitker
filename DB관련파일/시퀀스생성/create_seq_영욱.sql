4e36699b0a9f9b31ba4cafcac8ff4f7ea588aa1b
dnrdud1401 <76648429+dnrdud1401@users.noreply.github.com>
2021-01-06 09:02:57 +0900

    작업내용

---
+++ DB관련파일/시퀀스생성/create_seq_영욱.sql
@@ -0,0 +1,34 @@
+--■■■ 시퀀스(SEQUENCE) 생성 ■■■--
+--※시퀀스이름 : 테이블명간단히_SEQ
+
+-- 시퀀스 조회
+SELECT *
+FROM USER_SEQUENCES;
+
+-- ○ 스터디 신고 등록 테이블
+CREATE SEQUENCE STUDY_ACC_SEQ
+NOCACHE;
+
+-- ○ 스터디 신고 처리 테이블
+CREATE SEQUENCE STUDY_ACC_HAN_SEQ
+NOCACHE;
+
+-- ○ 스터디 경고 등록 테이블
+CREATE SEQUENCE STUDY_WAR_SEQ
+NOCACHE;
+
+-- ○ 욕설 테이블
+CREATE SEQUENCE CURSE_SEQ
+NOCACHE;
+
+-- ○ 고객센터(1:1문의) 테이블
+CREATE SEQUENCE ASK_SEQ
+NOCACHE;
+
+-- ○ 고객센터(공지사항) 테이블
+CREATE SEQUENCE NOTICE_SEQ
+NOCACHE;
+
+-- ○ 관리자 테이블
+CREATE SEQUENCE ADMIN_SEQ
+NOCACHE;
